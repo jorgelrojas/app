ab97c82d2ff784c379feb2b00d7f1489f8cdca68
jrojas <jorge.l.rojas@gmail.com>
2023-01-26 15:03:17 -0800

    gitingore

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # project/app/config.py
 
+
 import logging
+from functools import lru_cache
 
 from pydantic import BaseSettings
 
@@ -13,6 +15,7 @@ class Settings(BaseSettings):
     testing: bool = 0
 
 
+@lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
     return Settings()
